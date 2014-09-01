ccc0b6bb89a451fab4cf4bee8b438dcb3b8fea2d
Adam Taplin <aktaplin@hotmail.com>
2014-09-01 13:54:14 -0400

    benchmark before trying reactive template styles

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -4,15 +4,7 @@ Template.layout.tagListReturn = function() {
     Session.set('tags', JSON.parse(results.content));
   });
   return Session.get('tags');
-}
-
-Template.layout.tagBookmarksListReturn = function(tag) {
-  Meteor.call('getBookmarksByTag', tag, function(err, results) {
-    console.log(results.content);
-    Session.set('bookmarksByTag', JSON.parse(results.content));
-  });
-  return Session.get('bookmarksByTag');
-}
+};
 
 UI.registerHelper('addKeys', function(all) {
   return _.map(all, function(i, k) {
@@ -22,3 +14,15 @@ UI.registerHelper('addKeys', function(all) {
     };
   });
 });
+
+Template.layout.events = {
+  'click .tag': function() {
+    if (Session.get('bookmarksOn') == 'true') {
+      UI.remove(Template.test)
+    }
+    click = {}
+    click['target'] = event.target.id
+    UI.insert(UI.renderWithData(Template.test, click), $("#boing").get(0));
+    Session.set('bookmarksOn', 'true')
+  }
+};
